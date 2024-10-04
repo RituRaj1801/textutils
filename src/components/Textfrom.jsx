85efc88709fa8206cac0a99caf573ad9910535c9
RituRaj1801 <rituraj995kumar@gmail.com>
2024-10-04 22:19:41 +0530

    L3 added some preview in textfrom.js such as total word, character etc

---
+++ src/components/Textfrom.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+
+
+
+export default function Textfrom(props) {
+    const [text, setText] = useState("");
+    const onChangeHandler = (event) => {
+        setText(event.target.value); // Directly set the new value
+    }
+    const toUpperCase = () => {
+        var newtext = text.toUpperCase()
+        setText(newtext)
+    }
+
+    const toLowerCase = () => {
+        var newtext = text.toLowerCase()
+        setText(newtext)
+    }
+    return (
+        <>
+            <h3>{props.heading}</h3>
+            <div className="mb-3">
+                <textarea className="form-control" id="exampleFormControlTextarea1" rows="5" value={text} onChange={onChangeHandler} placeholder='Enetr your text for analyzse' />
+            </div>
+            <button className="btn btn-primary mx-1" onClick={toUpperCase}>Convert to UpperCase</button>
+            <button className="btn btn-primary" onClick={toLowerCase}>Convert to LowerCase</button>
+            <div className="container">
+                <h3>Text Summary</h3>
+                <p>Total character-{text.length}</p>
+                <p>Total Word-{ text==""?0: text.split(' ').length}</p>
+                <p>Total Time Taken-{ text==""?0: (0.008*text.split(' ').length.toFixed(2))}m</p>
+                <h3>Preview</h3>
+                <p>{text.trim()}</p>
+            </div>
+        </>
+    )
+}
