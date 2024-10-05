9407ed19fbb8a43e1dc4bd23187f754c9b2d82c6
RituRaj1801 <rituraj995kumar@gmail.com>
2024-10-05 18:14:24 +0530

    added a btn for dark mode in navbar but can implement in textare using props

--- src/App.js
+++ src/App.js
@@ -2,13 +2,42 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import Textfrom from './components/Textfrom';
+import { useState } from 'react';
 
 function App() {
+
+  const [modeDark, setModeDark] = useState(true)
+  const [modeBtnText, setModeBtnText] = useState("dark Mode")
+  const [myStyle, setMyStyle] = useState({
+    color: 'black',
+    backgroundColor: 'white'
+  })
+  const toggleMode = () => {
+    console.log("clicked")
+    if (modeDark) {
+      document.body.style.backgroundColor='black'
+      setMyStyle({
+        color: 'white',
+        backgroundColor: 'black'
+      })
+      setModeDark(false)
+      setModeBtnText('light mode')
+    }else{
+      document.body.style.backgroundColor='white'
+
+      setMyStyle({
+        color: 'black',
+        backgroundColor: 'white'
+      })
+      setModeDark(true)
+      setModeBtnText('dark mode')
+    }
+  }
   return (
     <>
-      <Navbar title="TextUtils"/>
-      <div className="container">
-        <Textfrom heading="Analyze your text"/>
+      <Navbar title="TextUtils" modeBtnText={modeBtnText} modeDark={modeDark}  toggleMode={toggleMode}/>
+      <div className="container" style={myStyle}   >
+        <Textfrom heading="Analyze your text"  modeDark={modeDark}/>
       </div>
     </>
   );
--- src/components/Textfrom.jsx
+++ src/components/Textfrom.jsx
@@ -18,19 +18,22 @@ export default function Textfrom(props) {
     }
     return (
         <>
-            <h3>{props.heading}</h3>
-            <div className="mb-3">
-                <textarea className="form-control" id="exampleFormControlTextarea1" rows="5" value={text} onChange={onChangeHandler} placeholder='Enetr your text for analyzse' />
-            </div>
-            <button className="btn btn-primary mx-1" onClick={toUpperCase}>Convert to UpperCase</button>
-            <button className="btn btn-primary" onClick={toLowerCase}>Convert to LowerCase</button>
-            <div className="container">
-                <h3>Text Summary</h3>
-                <p>Total character-{text.length}</p>
-                <p>Total Word-{ text==""?0: text.split(' ').length}</p>
-                <p>Total Time Taken-{ text==""?0: (0.008*text.split(' ').length.toFixed(2))}m</p>
-                <h3>Preview</h3>
-                <p>{text.trim()}</p>
+            <div className="border border-dark p-3 my-3">
+                <h3>{props.heading}</h3>
+                <div className="mb-3">
+                    <textarea className={`form-control bg-${props.modeDark ? "light" : "dark"} text-${props.modeDark ? "black" : "light"}`} id="exampleFormControlTextarea1" rows="5" value={text} onChange={onChangeHandler} placeholder='Enetr your text for analyzse' />
+                </div>
+                <button className="btn btn-primary mx-1" onClick={toUpperCase}>Convert to UpperCase</button>
+                <button className="btn btn-primary" onClick={toLowerCase}>Convert to LowerCase</button>
+                {/* <button className="btn btn-primary mx-1" onClick={props.toggleMode}>{props.modeBtnText}</button> */}
+                <div className="container">
+                    <h3>Text Summary</h3>
+                    <p>Total character-{text.length}</p>
+                    <p>Total Word-{text == "" ? 0 : text.split(' ').length}</p>
+                    <p>Total Time Taken-{text == "" ? 0 : (0.008 * text.split(' ').length.toFixed(2))}m</p>
+                    <h3>Preview</h3>
+                    <p>{text.trim()}</p>
+                </div>
             </div>
         </>
     )
